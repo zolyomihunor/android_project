6df7c030b61fb4ca28bc91075198ebc378fba74c
Hunor97 <57155682+Hunor97@users.noreply.github.com>
2020-11-25 14:30:53 +0200

    Fragment+BottomNavigationMenu

--- app/src/main/java/com/example/project_wheretoeat/MainActivity.kt
+++ app/src/main/java/com/example/project_wheretoeat/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.project_wheretoeat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.project_wheretoeat.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        //inflate layout
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        //set up BottomNavigation
+        val navController = findNavController(R.id.fragment)
+
+        val bottomNav = binding.bottomNavigationView
+        bottomNav?.setupWithNavController(navController)
+
     }
 }
